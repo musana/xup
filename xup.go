2e0f263941a5575e505ce04d623dfac5c308bde8
musana <musana@users.noreply.github.com>
2024-07-26 16:49:30 +0300

    Update xup.go

--- xup.go
+++ xup.go
@@ -1,47 +1,56 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "flag"
-    "regexp"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
 )
 
 func main() {
 
-    help := flag.Bool("help", false, "Show help")
+	help := flag.Bool("help", false, "Show help")
 	onlyIP := flag.Bool("onlyip", false, "Print only IP addresses")
 	flag.Parse()
 
-
-    if *help {
-		fmt.Println("Usage: masscan x.x.x.x/x | massparse")
-		fmt.Println("Options:")
-		fmt.Println("  -help     Show help")
-		fmt.Println("  -onlyip   Print only IP addresses")
-        fmt.Println("@musana | musana.net")
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error checking stdin:", err)
 		return
 	}
 
-    scanner := bufio.NewScanner(os.Stdin)
-    re := regexp.MustCompile(`Discovered open port (\d+)/(tcp|udp) on (\d+\.\d+\.\d+\.\d+)`)
-    
-        for scanner.Scan() {
-            line := scanner.Text()
-            match := re.FindStringSubmatch(line)
-            if match != nil {
-                ip := match[3]
-                port := match[1]
-                if *onlyIP {
-                    fmt.Println(ip)
-                }else {
-                fmt.Printf("%s:%s\n", ip, port)
-            }
-        }
-    }
+	if (stat.Mode()&os.ModeCharDevice) != 0 || *help {
+		printHelp()
+		return
+	}
 
-    if err := scanner.Err(); err != nil {
-        fmt.Fprintln(os.Stderr, "error reading input:", err)
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	re := regexp.MustCompile(`Discovered open port (\d+)/(tcp|udp) on (\d+\.\d+\.\d+\.\d+)`)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		match := re.FindStringSubmatch(line)
+		if match != nil {
+			ip := match[3]
+			port := match[1]
+			if *onlyIP {
+				fmt.Println(ip)
+			} else {
+				fmt.Printf("%s:%s\n", ip, port)
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+	}
+}
+
+func printHelp() {
+	fmt.Println("\nUsage: masscan x.x.x.x/x -p445 | xup")
+	fmt.Println("Options:")
+	fmt.Println("  -help     Show help")
+	fmt.Println("  -onlyip   Print only IP addresses")
+	fmt.Println("\n@musana | musana.net")
 }
